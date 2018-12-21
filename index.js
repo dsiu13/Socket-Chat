eb11f4e846cbb073c2031dd660b1fecd6bf89368
IamZombiez <dereksiu216@gmail.com>
2018-12-21 15:33:21 -0800

    stastarter

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
